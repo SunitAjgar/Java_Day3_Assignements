4a5f648b45cfeb944335e0a8bc3553302acd0f53
Sunit Ajgar <109612599+SunitAjgar@users.noreply.github.com>
2022-10-22 05:09:58 +0530

    Welcome to Employee Wage Computation Program

---
+++ EmployeeWage.java
@@ -0,0 +1,9 @@
+package com.blz.assignment.day3;
+
+import java.util.Random;
+
+public class EmployeeWage {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+	}
+}
