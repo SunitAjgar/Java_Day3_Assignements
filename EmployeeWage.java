7b5d4ab9c63032917f95bec558c685afde45c797
Sunit Ajgar <109612599+SunitAjgar@users.noreply.github.com>
2022-10-22 05:42:19 +0530

    Calculating Daily Employee Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,7 +5,23 @@ import java.util.Random;
 public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-	    Random random = new Random();
-	    int randomNum = random.nextInt(3);
+		int EmpWagePerHr = 20;
+		int fullWorkingHr = 8;
+		int partTimeWage = 10;
+		int partWorkingHr = 4;
+		int fullWage;
+		int partTimeWage1;
+		Random random = new Random();
+		int randomNum = random.nextInt(3);
+		switch (randomNum) {
+		case 0:
+		System.out.println("Employee is absent");
+		break;
+		case 1:
+        System.out.println("Employee is present");
+        fullWage = EmpWagePerHr * fullWorkingHr;
+        System.out.println("The Daily Employee Wage for full time Employee is  "+fullWage +"$");
+        break;
+		}
 	}
 }
