3dc8572009ab5a5198aa81e0f91f67be940548af
Sunit Ajgar <109612599+SunitAjgar@users.noreply.github.com>
2022-10-22 06:28:31 +0530

    Calculating Wages for a Month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,9 +1,15 @@
 package com.blz.assignment.day3;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class EmployeeWage {
+	static int monthlyWage;
+	static int monthlyHrs = 100;
+	static int monthlyDays = 20;
+
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to Employee Wage Computation Program");
 		int EmpWagePerHr = 20;
 		int fullWorkingHr = 8;
@@ -11,6 +17,7 @@ public class EmployeeWage {
 		int partWorkingHr = 4;
 		int fullWage;
 		int partTimeWage1;
+
 		Random random = new Random();
 		int randomNum = random.nextInt(3);
 		switch (randomNum) {
@@ -18,14 +25,19 @@ public class EmployeeWage {
 		System.out.println("Employee is absent");
 		break;
 		case 1:
-        System.out.println("Employee is present");
-        fullWage = EmpWagePerHr * fullWorkingHr;
-        System.out.println("The Daily Employee Wage for full time Employee is  "+fullWage +"$");
-        break;
+		System.out.println("Employee is present");
+		fullWage = EmpWagePerHr * fullWorkingHr;
+		System.out.println("The Daily Employee Wage for full time Employee is  " + fullWage + "$");
+		System.out.println("Calculating Wage for a month");
+		monthlyWage = monthlyDays * EmpWagePerHr * fullWorkingHr;
+		System.out.println("The monthly wage of a full time employee is  " + monthlyWage + "$");
+		break;
+		
 		case 2:
 		System.out.println("Employee is partime present");
 		partTimeWage1 = partTimeWage * partWorkingHr;
-		System.out.println("The Daily Wage for the Part Time Employee is  "+partTimeWage1 +"$");
+		System.out.println("The Daily Wage for the Part Time Employee is  " + partTimeWage1 + "$");
+		break;
 		}
 	}
 }
